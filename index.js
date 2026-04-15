cecffe5f62c74cafa61eb6a0e848bd22285d6072
Twy <matheusg.stein@gmail.com>
2026-04-15 08:43:17 -0300

    revisão da logica do index

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
-import Sorvete from "./models/Sorvete.js"
-import Receita from "./models/Receita.js"
-import Custo from "./models/Custo.js"
-const divRes = document.getElementById('divRes')
+import Sorvete from "./models/Sorvete.js";
+import Receita from "./models/Receita.js";
+import Custo from "./models/Custo.js";
+const divRes = document.getElementById("divRes");
 
-const tamanho = new Sorvete(0, 0)
-const pesoSorvete = tamanho.calcularVolume()
+// const peso = Number(document.getElementById('peso'))
 
-const receita = new Receita() 
-const qtdeIngredientes = receita.calcularQtdeIngredientes()
-const qtdeSorvete = receita.calcularQtdeSorvete(pesoSorvete)
-    
-const custo = new Custo()
-const precosIngredientes = custo.calcularCusto(qtdeIngredientes)
+const tamanho = new Sorvete(peso);
+const pesoSorvete = tamanho.conversaoMassa();
 
+const receita = new Receita();
+const qtdeIngredientes = receita.calcularQtdeIngredientes();
+const qtdeSorvete = receita.calcularQtdeSorvetes(peso);
 
-divRes.innerHTML = `<p>A quantidade de sorvete é: <strong>${qtdeSorvete}</strong></p>`
+const custo = new Custo();
+const precosIngredientes = custo.calcularCusto(qtdeIngredientes);
 
-divRes.innerHTML = `<p>--- Quantidade de Ingredientes ---</p> 
-                    <pre>${JSON.stringify(qtdeIngredientes, null, 2)}</pre>`
+divRes.innerHTML += `<p>A quantidade de sorvete é: <strong>${qtdeSorvete}</strong></p>`;
 
-divRes.innerHTML =`<p>--- Custo dos Ingredientes (R$) ---</p>
-                   <pre>${JSON.stringify(precosIngredientes, null, 2)}</pre>`
+divRes.innerHTML += `<p>--- Quantidade de Ingredientes ---</p> 
+                    <pre>${JSON.stringify(qtdeIngredientes, null, 2)}</pre>`;
 
-divRes.innerHTML =`<p>--- Custo por pote (R$) ---</p>
-<pre>${JSON.stringify(custo.calcularCustoPorPote, null, 2)}</pre>`
+divRes.innerHTML += `<p>--- Custo dos Ingredientes (R$) ---</p>
+                   <pre>${JSON.stringify(precosIngredientes, null, 2)}</pre>`;
 
-divRes.innerHTML = ` <p>O custo total de produção é: <strong>R$ ${custo.totalCusto}</strong></p>
-                     <p>O custo de massa por sorvete é: <strong>R$ ${(custo.totalCusto / qtdeSorvete).toFixed(2)}</strong></p>`
+divRes.innerHTML += `<p>--- Custo por pote (R$) ---</p>
+<pre>${JSON.stringify(custo.calcularCustoPorPote(), null, 2)}</pre>`;
+
+divRes.innerHTML += ` <p>O custo total de produção é: <strong>R$ ${custo.totalCusto()}</strong></p>
+                     <p>O custo de massa por sorvete é: <strong>R$ ${(custo.totalCusto() / qtdeSorvete).toFixed(2)}</strong></p>`
